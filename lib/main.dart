869b14dc3868cee58fe55003b24931f6877097b6
ForwardOoh <soa9719@naver.com>
2023-02-15 01:39:52 +0900

    feat: 게시판 공지 카드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:danvery/model/user_model.dart';
 import 'package:danvery/pages/home_page.dart';
 import 'package:danvery/pages/login_page.dart';
 import 'package:danvery/pages/timetable_page.dart';
+import 'package:danvery/pages/board_page.dart';
 import 'package:danvery/settings/palette.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -15,14 +16,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 
 const apiHost = 'https://api.dku54play.site';
-  //'http://133.186.218.8:8080'
-  //'https://api.dku54play.site'
+//'http://133.186.218.8:8080'
+//'https://api.dku54play.site'
 
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context){
+  HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
 
@@ -31,7 +33,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   State<StatefulWidget> createState() {
@@ -39,137 +41,140 @@ class MyApp extends StatefulWidget{
   }
 }
 
-class _MyApp extends State<MyApp>{
-
-  static UserModel userModel = UserModel(
-      subjects: [
-        SubjectModel(name: "확률과 통계", startTime: '09:00', endTime: '10:30', days: ["월", "화"]),
-        SubjectModel(name: "선형 대수", startTime: '11:00', endTime: '12:30', days: ["수"]),
-      ],
-      name: "이재민",
-      major: "소프트웨어학과",
-      studentNumber: "32193419"
-  );
+class _MyApp extends State<MyApp> {
+  static UserModel userModel = UserModel(subjects: [
+    SubjectModel(
+        name: "확률과 통계", startTime: '09:00', endTime: '10:30', days: ["월", "화"]),
+    SubjectModel(
+        name: "선형 대수", startTime: '11:00', endTime: '12:30', days: ["수"]),
+  ], name: "이재민", major: "소프트웨어학과", studentNumber: "32193419");
 
   int page = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.light,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          unselectedItemColor: Palette.dark,
-          selectedItemColor: Palette.dark,
-        ),
-        iconTheme: IconThemeData(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            unselectedItemColor: Palette.dark,
+            selectedItemColor: Palette.dark,
+          ),
+          iconTheme: IconThemeData(
             color: Palette.dark,
+          ),
         ),
-      ),
-      home: FutureBuilder<LoginModel?>(
-        future: login(),
-        builder: (BuildContext context, AsyncSnapshot<LoginModel?> snapshot){
-          if(snapshot.hasError){
-            return Center(
-              child: Text(
-                'Error: ${snapshot.error}',
-                style: const TextStyle(fontSize: 15),
-              ),
-            );
-          }else if(snapshot.hasData == false){
-            return const Center(child: CircularProgressIndicator());
-          }else if(snapshot.data == null){
-            return Container();
-          }else{
-            return SafeArea(
-              child: Scaffold(
-                backgroundColor: Palette.white,
-                body: Padding(
-                  padding: EdgeInsets.only(bottom: 60),
-                  child: IndexedStack(
-                    index: page,
-                    children: [
-                      HomePage(
-                        loginModel: snapshot.data!,
-                        userModel: userModel,
-                      ),
-                      TimetablePage(
-                        userModel: userModel
-                      ),
-                      LoginPage()
-                    ],
-                  ),
+        home: FutureBuilder<LoginModel?>(
+          future: login(),
+          builder: (BuildContext context, AsyncSnapshot<LoginModel?> snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: const TextStyle(fontSize: 15),
                 ),
-                bottomNavigationBar: Container(
-                  height: 60,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-                    boxShadow: [
-                      BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 1),
-                    ],
-                  ),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      topLeft: Radius.circular(20)
-                    ),
-                    child: BottomNavigationBar(
-                      onTap: (value){
-                        setState(() {
-                          page = value;
-                        });
-                      },
-                      //currentIndex: page,
-                      backgroundColor: Palette.white,
-                      showSelectedLabels: false,
-                      showUnselectedLabels: false,
-                      items: [
-                        BottomNavigationBarItem(
-                          icon: Image.asset("assets/icons/bottom_nav/home_icon.png",scale: 2,),
-                          label: "홈",
+              );
+            } else if (snapshot.hasData == false) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.data == null) {
+              return Container();
+            } else {
+              return SafeArea(
+                child: Scaffold(
+                  backgroundColor: Palette.white,
+                  body: Padding(
+                    padding: EdgeInsets.only(bottom: 60),
+                    child: IndexedStack(
+                      index: page,
+                      children: [
+                        HomePage(
+                          loginModel: snapshot.data!,
+                          userModel: userModel,
                         ),
-                        BottomNavigationBarItem(
-                          icon: Image.asset("assets/icons/bottom_nav/timetable_icon.png",scale: 2),
-                          label: "시간표"
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Image.asset("assets/icons/bottom_nav/bus_icon.png",scale: 2),
-                          label: "버스"
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Image.asset("assets/icons/bottom_nav/board_icon.png",scale: 2),
-                          label: "게시판"
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Image.asset("assets/icons/bottom_nav/setting_icon.png",scale: 2),
-                          label: "설정"
-                        ),
+                        TimetablePage(userModel: userModel),
+                        LoginPage(),
+                        BoardPage(),
                       ],
                     ),
                   ),
+                  bottomNavigationBar: Container(
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          topLeft: Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black38,
+                            spreadRadius: 0,
+                            blurRadius: 1),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          topLeft: Radius.circular(20)),
+                      child: BottomNavigationBar(
+                        onTap: (value) {
+                          setState(() {
+                            page = value;
+                          });
+                        },
+                        //currentIndex: page,
+                        backgroundColor: Palette.white,
+                        showSelectedLabels: false,
+                        showUnselectedLabels: false,
+                        items: [
+                          BottomNavigationBarItem(
+                            icon: Image.asset(
+                              "assets/icons/bottom_nav/home_icon.png",
+                              scale: 2,
+                            ),
+                            label: "홈",
+                          ),
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/bottom_nav/timetable_icon.png",
+                                  scale: 2),
+                              label: "시간표"),
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/bottom_nav/bus_icon.png",
+                                  scale: 2),
+                              label: "버스"),
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/bottom_nav/board_icon.png",
+                                  scale: 2),
+                              label: "게시판"),
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/bottom_nav/setting_icon.png",
+                                  scale: 2),
+                              label: "설정"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  extendBody: true,
                 ),
-                extendBody: true,
-              ),
-            );
-          }
-        },
-      )
-    );
+              );
+            }
+          },
+        ));
   }
 
   Future<LoginModel?> login() async {
-
     http.Response loginInfoResponse = await http.post(
-        Uri.parse('$apiHost/api/users/login'),
-        headers: {"Content-Type": "application/json"},
-        body: '''{\n        "classId":"17011092",\n        "password":"99730567"\n}''',
+      Uri.parse('$apiHost/api/users/login'),
+      headers: {"Content-Type": "application/json"},
+      body:
+          '''{\n        "classId":"17011092",\n        "password":"99730567"\n}''',
     );
 
-    if(loginInfoResponse.statusCode == 200){
+    if (loginInfoResponse.statusCode == 200) {
       print(loginInfoResponse.statusCode);
       final loginInfoParsed = json.decode(loginInfoResponse.body);
       final LoginModel loginInfo = LoginModel.formJson(loginInfoParsed);
@@ -178,5 +183,4 @@ class _MyApp extends State<MyApp>{
 
     return null;
   }
-
 }
--- lib/pages/board_page.dart
+++ lib/pages/board_page.dart
@@ -1,6 +1,17 @@
+import 'dart:convert';
+import 'package:danvery/model/login_model.dart';
+import 'package:danvery/model/petition_model.dart';
+import 'package:danvery/model/post_model.dart';
+import 'package:danvery/model/user_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../dto/board_list_dto.dart';
+import '../main.dart';
+import '../settings/palette.dart';
 
-class BoardPage extends StatefulWidget{
+class BoardPage extends StatefulWidget {
   const BoardPage({super.key});
 
   @override
@@ -8,16 +19,239 @@ class BoardPage extends StatefulWidget{
     // TODO: implement createState
     return _BoardPage();
   }
-
 }
 
-class _BoardPage extends State<BoardPage>{
+class _BoardPage extends State<BoardPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '자유게시판',
+          style: TextStyle(fontSize: 20, color: Palette.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: null),
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: null),
+          IconButton(icon: Icon(Icons.mode_edit_outline), onPressed: null),
+        ],
+        backgroundColor: Palette.white,
+      ),
+      body: Column(
+        children: [
+          NoticeCard(
+              category: "공지",
+              title: "셔틀버스 시간 변경 안내",
+              commentCnt: 8,
+              likeCnt: 9),
+          NoticeCard(
+              category: "인기",
+              title: "오늘",
+              imageCnt: 2,
+              commentCnt: 2,
+              likeCnt: 24),
+          PostCard(
+              category: "자유게시판",
+              title: "졸업하면",
+              content: "돈 많이 벌어야지",
+              imageCnt: 2,
+              commentCnt: 2,
+              likeCnt: 24),
+          PostCard(
+              category: "자유게시판",
+              title: "졸업하면",
+              content: "돈 많이 벌어야지",
+              imageCnt: 2,
+              commentCnt: 2,
+              likeCnt: 24),
+        ],
+      ),
     );
   }
+}
 
+class NoticeCard extends StatelessWidget {
+  final String category;
+  final String title;
+  final int? imageCnt;
+  final int? commentCnt;
+  final int? likeCnt;
+
+  const NoticeCard(
+      {super.key,
+      required this.category,
+      required this.title,
+      this.imageCnt,
+      this.commentCnt,
+      this.likeCnt});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          elevation: 4.0,
+          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+            child: ListTile(
+              leading: Text(
+                category,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Palette.blue),
+              ),
+              title: Text(
+                title,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Palette.black),
+              ),
+              trailing: FittedBox(
+                fit: BoxFit.fill,
+                child: Row(
+                  children: <Widget>[
+                    Wrap(
+                      spacing: 12,
+                      children: [
+                        imageCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.image_outlined),
+                                  Text(
+                                    imageCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                        commentCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.chat_outlined),
+                                  Text(
+                                    commentCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                        likeCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.thumb_up_off_alt),
+                                  Text(
+                                    likeCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class PostCard extends StatelessWidget {
+  final String category;
+  final String title;
+  final String content;
+  final String? thumbnailUrl;
+  final int? imageCnt;
+  final int? commentCnt;
+  final int? likeCnt;
+
+  const PostCard(
+      {super.key,
+      required this.category,
+      required this.title,
+      required this.content,
+      this.thumbnailUrl,
+      this.imageCnt,
+      this.commentCnt,
+      this.likeCnt});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListTile(
+          isThreeLine: true,
+          title: Text(
+            title,
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Palette.black),
+          ),
+          subtitle: Text(content),
+          trailing: Column(
+            children: [
+              FittedBox(
+                fit: BoxFit.fill,
+                child: Row(
+                  children: <Widget>[
+                    Wrap(
+                      spacing: 12,
+                      children: [
+                        imageCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.image_outlined),
+                                  Text(
+                                    imageCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                        commentCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.chat_outlined),
+                                  Text(
+                                    commentCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                        likeCnt != null
+                            ? Row(
+                                children: [
+                                  Icon(Icons.thumb_up_off_alt),
+                                  Text(
+                                    likeCnt.toString(),
+                                    style: TextStyle(
+                                        fontSize: 20, color: Palette.grey),
+                                  ),
+                                ],
+                              )
+                            : Row(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
 }
