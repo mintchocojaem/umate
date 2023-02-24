46214d9c9f886daf5e319ac6f204d0b8a9500811
ForwardOoh <soa9719@naver.com>
2023-02-24 18:42:12 +0900

    feat: 청원게시판 게시물 페이지 추가

--- lib/app/ui/pages/petition_post_page.dart
+++ lib/app/ui/pages/petition_post_page.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../theme/app_colors.dart';
 import '../theme/app_text_theme.dart';
 
-class PetitionBoardPage extends StatefulWidget {
+class PetitionPostPage extends StatefulWidget {
   @override
-  _PetitionBoardPageState createState() => _PetitionBoardPageState();
+  _PetitionPostPageState createState() => _PetitionPostPageState();
 }
 
-class _PetitionBoardPageState extends State<PetitionBoardPage> {
+class _PetitionPostPageState extends State<PetitionPostPage> {
   @override
   Widget build(BuildContext context) {
-    final deviceWidth = MediaQuery.of(context).size.width;
-    final categoryButtonWidth = deviceWidth / 5.0;
-    final categoryButtonPadding = (deviceWidth - 40.0) / 24.0;
-
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(100.0),
@@ -29,270 +26,254 @@ class _PetitionBoardPageState extends State<PetitionBoardPage> {
                 onPressed: () => {Navigator.pop(context)}),
             title: Text(
               '청원게시판',
-              style: titleStyle,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: black,
+              ),
             ),
-            actions: [
-              IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: grey,
-                  ),
-                  onPressed: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SearchBoardPage()))
-                      }),
-              IconButton(
-                  icon: Icon(
-                    Icons.mode_edit_outline,
-                    color: grey,
-                  ),
-                  onPressed: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => NewpostPage()))
-                      }),
-            ],
             centerTitle: true,
             backgroundColor: Colors.transparent,
             elevation: 0,
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              SlidingButtonBar(),
-              PetitionCard(
-                title: "셔틀버스 실시간 위치 정보 공유 청원",
-                dueDate: "D - 30",
-                petitionPeriod: "2023-01-15 ~ 2023-02-15",
-                numberOfPeople: "143",
-                petitionStatus: "답변완료",
-              ),
-              PetitionCard(
-                title: "셔틀버스 실시간 위치 정보 공유 청원",
-                dueDate: "D - 30",
-                petitionPeriod: "2023-01-15 ~ 2023-02-15",
-                numberOfPeople: "143",
-                petitionStatus: "청원 중",
-              ),
-              PetitionCard(
-                title: "셔틀버스 실시간 위치 정보 공유 청원",
-                dueDate: "D - 30",
-                petitionPeriod: "2023-01-15 ~ 2023-02-15",
-                numberOfPeople: "143",
-                petitionStatus: "답변 대기 중",
-              ),
-              PetitionCard(
-                title: "셔틀버스 실시간 위치 정보 공유 청원",
-                dueDate: "D - 30",
-                petitionPeriod: "2023-01-15 ~ 2023-02-15",
-                numberOfPeople: "143",
-                petitionStatus: "답변완료",
-              ),
-              PetitionCard(
-                title: "셔틀버스 실시간 위치 정보 공유 청원",
-                dueDate: "D - 30",
-                petitionPeriod: "2023-01-15 ~ 2023-02-15",
-                numberOfPeople: "143",
-                petitionStatus: "기간만료",
-              ),
-            ],
-          ),
-        ),
+      body: const PetitionPostView(
+        title: '여기는 제목 1',
+        poster: '123',
+        content: '여기는 내용이 나올 곳',
+        publishDate: " 날짜는 01/15 12:39",
+        commentCnt: 3,
+        likeCnt: 10,
       ),
     );
   }
 }
 
-class StatusFillter extends StatelessWidget {
-  final double categoryButtonWidth;
-  final double categoryButtonPadding;
-  final String fillterText;
+class PetitionPostView extends StatelessWidget {
+  final String title;
+  final String poster; // 글쓴이
+  final String content;
+  final String? publishDate; // null 수정 예정
+  final String? imageUrl;
+  final int? commentCnt;
+  final int? likeCnt;
 
-  const StatusFillter({
-    super.key,
-    required this.categoryButtonWidth,
-    required this.categoryButtonPadding,
-    required this.fillterText,
-  });
+  const PetitionPostView(
+      {super.key,
+      required this.title,
+      required this.poster,
+      required this.content,
+      this.publishDate,
+      this.imageUrl,
+      this.commentCnt,
+      this.likeCnt});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: categoryButtonPadding, bottom: 30.0),
-      child: SizedBox(
-        width: categoryButtonWidth,
-        height: 30,
-        child: ElevatedButton(
-          child: Text(
-            fillterText,
+    void dialog() {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              icon: Icon(
+                Icons.check_circle_outline,
+                color: blue,
+                size: 30.0,
+              ),
+              title: Text("동의가 완료되었습니다."),
+            );
+          });
+    }
+
+    TextEditingController agreementTextController = TextEditingController();
+    String agreement = "동의합니다.";
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "[학생복지]",
             style: TextStyle(
+              color: lightBlue,
+              fontSize: 14.0,
+            ),
+          ),
+          Text(
+            "셔틀버스 실시간 위치 정보 공유 청원",
+            style: TextStyle(
+              color: black,
+              fontSize: 18.0,
               fontWeight: FontWeight.bold,
             ),
           ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: blue,
-            foregroundColor: white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-          ),
-          onPressed: () {},
-        ),
-      ),
-    );
-  }
-}
-
-class PetitionCard extends StatelessWidget {
-  final String title; // 제목
-  final String dueDate; // 마감 일자
-  final String petitionPeriod; // 청원 기간
-  final String numberOfPeople; // 참여 인원
-  final String petitionStatus; // 청원 상태
-
-  const PetitionCard({
-    super.key,
-    required this.title,
-    required this.dueDate,
-    required this.petitionPeriod,
-    required this.numberOfPeople,
-    required this.petitionStatus,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 15.0),
-      child: InkWell(
-        onTap: () => {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => PetitionPostPage()))
-        },
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            side: petitionStatus == "답변완료" // 답변완료의 경우 테두리가 파란색으로 강조
-                ? BorderSide(
-                    color: lightBlue,
-                  )
-                : BorderSide.none,
-          ),
-          elevation: petitionStatus == "답변완료" ? 0 : 4.0, // 답변완료의 경우 카드 그림자 없음
-
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
               children: [
-                Text(
-                  "[학생복지]",
-                  style: TextStyle(
-                    color: lightBlue,
-                    fontSize: 12.0,
-                  ),
-                ),
-                Text(
-                  title,
-                  style: TextStyle(
-                    color: black,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                Expanded(
                   child: Text(
-                    dueDate,
+                    "청원기간",
                     style: TextStyle(
                       color: grey,
-                      fontSize: 18.0,
+                      fontSize: 16.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  flex: 1,
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        "청원기간",
-                        style: TextStyle(
-                          color: grey,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      flex: 4,
+                Expanded(
+                  child: Text(
+                    "2023-01-15 ~ 2023-02-15",
+                    style: TextStyle(
+                      color: grey,
+                      fontSize: 16.0,
                     ),
-                    Expanded(
-                      child: Text(
-                        "참여인원",
-                        style: TextStyle(
-                          color: grey,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      flex: 1,
-                    ),
-                    Expanded(
-                      child: Text(
-                        numberOfPeople + "명",
-                        style: TextStyle(
-                          color: grey,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      flex: 1,
-                    ),
-                  ],
+                  ),
+                  flex: 4,
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        petitionPeriod,
-                        style: TextStyle(
-                          color: grey,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      flex: 4,
+                Expanded(
+                  child: Text(
+                    "청원상태",
+                    style: TextStyle(
+                      color: grey,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                      child: Text(
-                        "청원상태",
-                        style: TextStyle(
-                          color: grey,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      flex: 1,
+                  ),
+                  flex: 1,
+                ),
+                Expanded(
+                  child: Text(
+                    "답변완료",
+                    style: TextStyle(
+                      color: blue,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                      child: Text(
-                        petitionStatus,
-                        style: TextStyle(
-                          color: blue,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      flex: 1,
-                    ),
-                  ],
+                  ),
+                  flex: 1,
                 ),
               ],
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Divider(thickness: 1, color: brightGrey),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 40.0),
+            child: Text(content),
+          ),
+          Row(
+            children: [
+              Text(
+                "참여인원 ",
+                style: TextStyle(
+                  color: grey,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "10명",
+                style: TextStyle(
+                  color: blue,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 50,
+                    child: TextField(
+                      controller: agreementTextController,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 3,
+                            color: brightGrey,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 3,
+                            color: brightGrey,
+                          ),
+                        ),
+                        hintText: '동의합니다.',
+                        hintStyle: TextStyle(
+                          color: brightGrey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 50,
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: ElevatedButton(
+                      child: Text(
+                        "동의",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () => {dialog()},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: blue,
+                        foregroundColor: white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(thickness: 5, color: brightGrey),
+
+          //여기서부터 댓글 위젯
+          ListTile(
+            title: Text(
+              "OO과",
+              style: TextStyle(
+                color: black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              "동의합니다.",
+              style: TextStyle(
+                color: black,
+                fontSize: 18.0,
+              ),
+            ),
+            trailing: Text(
+              "01/15 12:39",
+              style: TextStyle(
+                color: lightGrey,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: brightGrey,
+          ),
+        ],
       ),
     );
   }
