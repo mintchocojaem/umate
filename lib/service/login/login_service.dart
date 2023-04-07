6ea9355e00c451a8911cf63288522a01a6f17862
Jaem <91131509+chocojaem@users.noreply.github.com>
2023-04-07 17:02:41 +0900

    Login 추가 리팩토링

--- lib/service/login/login_service.dart
+++ lib/service/login/login_service.dart
@@ -21,77 +21,65 @@ class LoginService extends GetxService {
   final GetStorage _box = GetStorage();
 
   Future<void> login(String classId, String password) async {
-    await _loginRepository
-        .login(classId: classId, password: password)
-        .then((apiResponse) async {
-      if (apiResponse.success) {
-        loginModel = (apiResponse.data as LoginModel).obs;
-        _box.write("accessToken", apiResponse.data.accessToken);
-        _box.write("refreshToken", apiResponse.data.refreshToken);
-        isLogin.value = true;
-      } else {
-        isLogin.value = false;
-        GetXSnackBar(
-          type: GetXSnackBarType.customError,
-          title: "로그인 실패",
-          content: apiResponse.message,
-        ).show();
-      }
-    });
+    final apiResponse =
+        await _loginRepository.login(classId: classId, password: password);
+    if (apiResponse.success) {
+      loginModel = (apiResponse.data as LoginModel).obs;
+      _box.write("accessToken", apiResponse.data.accessToken);
+      _box.write("refreshToken", apiResponse.data.refreshToken);
+      isLogin.value = true;
+    } else {
+      isLogin.value = false;
+      GetXSnackBar(
+        type: GetXSnackBarType.customError,
+        title: "로그인 실패",
+        content: apiResponse.message,
+      ).show();
+    }
   }
 
   Future<void> autoLogin(String accessToken, String refreshToken) async {
-    await _loginRepository
-        .reissueToken(accessToken: accessToken, refreshToken: refreshToken)
-        .then((token) async {
-      if (token.success) {
+    final token = await _loginRepository.reissueToken(
+        accessToken: accessToken, refreshToken: refreshToken);
+    if (token.success) {
+      final TokenModel tokenModel = token.data as TokenModel;
 
-        final TokenModel tokenModel = token.data as TokenModel;
-
-        await _getUserInfo(token.data.accessToken).then((userInfo){
-          final UserInfoModel? userInfoModel = userInfo;
-          if(userInfoModel != null){
-            loginModel= LoginModel(
-              accessToken: tokenModel.accessToken,
-              refreshToken: tokenModel.refreshToken,
-              studentId: userInfoModel.studentId,
-              username: userInfoModel.username,
-              major: userInfoModel.major,
-              nickname: userInfoModel.nickname,
-              department: userInfoModel.department,
-              admin: false,
-            ).obs;
-            isLogin.value = true;
-
-          }else{
-            isLogin.value = false;
-          }
-
-        });
+      final userInfo = await _getUserInfo(tokenModel.accessToken);
+      if (userInfo != null) {
+        loginModel = LoginModel(
+          accessToken: tokenModel.accessToken,
+          refreshToken: tokenModel.refreshToken,
+          studentId: userInfo.studentId,
+          username: userInfo.username,
+          major: userInfo.major,
+          nickname: userInfo.nickname,
+          department: userInfo.department,
+          admin: false,
+        ).obs;
+        isLogin.value = true;
       } else {
         isLogin.value = false;
-       _box.remove("accessToken");
-       _box.remove("refreshToken");
       }
-    });
+    } else {
+      isLogin.value = false;
+      _box.remove("accessToken");
+      _box.remove("refreshToken");
+    }
   }
 
   Future<UserInfoModel?> _getUserInfo(String accessToken) async {
-    return await _loginRepository
-        .getUserInfo(accessToken: accessToken)
-        .then((apiResponse) async {
-      if (apiResponse.success) {
-        final UserInfoModel userInfoModel = apiResponse.data as UserInfoModel;
-        return userInfoModel;
-      } else {
-        GetXSnackBar(
-          type: GetXSnackBarType.customError,
-          title: "로그인 실패",
-          content: apiResponse.data.message,
-        ).show();
-        return null;
-      }
-    });
+    final apiResponse =
+        await _loginRepository.getUserInfo(accessToken: accessToken);
+    if (apiResponse.success) {
+      final UserInfoModel userInfoModel = apiResponse.data as UserInfoModel;
+      return userInfoModel;
+    } else {
+      GetXSnackBar(
+        type: GetXSnackBarType.customError,
+        title: "로그인 실패",
+        content: apiResponse.data.message,
+      ).show();
+      return null;
+    }
   }
-
 }
