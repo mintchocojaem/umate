4ec255ec1a8a1aea9404287922a102a2c82f7627
Jaem <91131509+DarkChocoJaem@users.noreply.github.com>
2024-03-01 03:56:54 +0900

    dependency update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
